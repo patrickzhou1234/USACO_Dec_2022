71a9ac5f0f32318c4dd95bfd518cacd2e2dc48f2
patrickzhou1234 <patrickchang44543@gmail.com>
2022-12-17 14:25:09 -0800

    abit ;-;

---
+++ reverseengineering/main.cpp
@@ -0,0 +1,20 @@
+// http://www.usaco.org/index.php?page=viewproblem&cpid=1241
+#include <iostream>
+
+using namespace std;
+
+#define MAX_INT 2147483647
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i=0;i<t;i++) {
+        int n, m;
+        cin >> n >> m;
+        for (int j=0;j<m;j++) {
+            
+        }
+    }
+    system("pause");
+    return 0;
+}
